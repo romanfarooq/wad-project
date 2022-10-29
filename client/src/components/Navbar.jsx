3d0044d21da1cadb585a2a3cc77383639b8dd1d7
Roman Farooq <TheRomanFarooq@gmail.com>
2022-10-29 20:15:42 +0500

    made side navbar responsive

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,20 +9,20 @@ function Navbar() {
     <>
       <nav className="fixed top-0 bottom-0 left-0 w-1/5 bg-[#f5f6fc] rounded-r-3xl shadow-inner">
         <div className="flex m-5 pb-3 border-b-2 border-blue-100">
-          <FcDocument className="text-5xl" />
-          <h1 className="text-2xl m-auto ml-1 text-[#153a75] font-extrabold">
+          <FcDocument className="text-5xl m-auto sm:m-0 md:text-4xl lg:text-5xl" />
+          <h1 className="hidden sm:block sm:text-sm md:text-base lg:text-lg xl:text-2xl m-auto ml-1 text-[#153a75] font-extrabold">
             TO-DO APP
           </h1>
         </div>
-        <div className="flex h-24 justify-center items-center">
+        <div className="flex h-16 flex-col xl:flex-row sm:h-32 xl:h-24 justify-center items-center">
           <img
-            className="rounded-full h-16 w-16 p-1 border-2 border-gray-100 shadow-sm"
+            className="rounded-full h-10 sm:h-16 w-10 sm:w-16 p-1 border-2 border-gray-100 shadow-sm"
             src={ProfilePicture}
             alt="profile-pic"
           />
-          <div className="flex flex-col justify-center text-[#153a75]">
+          <div className="hidden sm:flex text-center xl:text-left flex-col justify-center text-[#153a75]">
             <span className="font-semibold">Roman Farooq</span>
-            <small>TheRomanFarooq@gmail.com</small>
+            <small className="text-xs">TheRomanFarooq@gmail.com</small>
           </div>
         </div>
         <div className="flex flex-col gap-3">
@@ -33,33 +33,40 @@ function Navbar() {
               end
               className={({ isActive }) =>
                 isActive
-                  ? 'flex gap-2 px-5 py-2 rounded-r-md bg-[#ebecf5] border-r-8 border-[#234d92] text-[#153a75] font-bold'
-                  : 'flex gap-2 px-5 py-2 rounded-r-md hover:bg-[#ebecf5] hover:border-r-8 transition duration-200 border-[#234d92] text-[#153a75] font-medium hover:font-bold'
+                  ? 'flex items-center gap-2 px-2 justify-center sm:justify-start md:px-5 py-2 rounded-r-md bg-[#ebecf5] border-r-8 border-[#234d92] text-[#153a75] font-bold'
+                  : 'flex items-center gap-2 px-2 justify-center sm:justify-start md:px-5 py-2 rounded-r-md hover:bg-[#ebecf5] hover:border-r-8 transition duration-200 border-[#234d92] text-[#153a75] font-medium hover:font-bold'
               }
             >
               {item.icon}
-              <span className="text-lg">{item.name}</span>
+              <span className="hidden sm:block md:text-base lg:text-lg">
+                {item.name}
+              </span>
             </NavLink>
           ))}
         </div>
-        <div className="fixed bottom-0 left-0">
-          <div className="flex gap-2 px-5 py-4">
-            <MdDarkMode className="text-2xl mt-1 text-[#2196F3]" />
-            <span className="text-xl font-medium text-[#153a75]">
-              Dark Mode
-            </span>
-            <label
-              htmlFor="checkbox"
-              className="relative inline-block w-10 h-5 ml-1 mt-[5px]"
-            >
-              <input
-                type="checkbox"
-                id="checkbox"
-                className="opacity-0 w-0 h-0"
-                onClick={() => console.log('clicked')}
-              />
-              <span className="slider round"></span>
-            </label>
+        <div className="fixed bottom-0 left-0 w-1/5">
+          <div className="flex flex-col w-full l-md:flex-row gap-2 px-3 lg:px-5 py-4">
+            <MdDarkMode
+              className="text-2xl m-auto sm:m-0 sm:mt-1 cursor-pointer sm:cursor-default text-[#2196F3]"
+              onClick={() => console.log('clicked')}
+            />
+            <div className="hidden sm:flex">
+              <span className="block my-auto text-xs md:text-sm lg:text-base xl:text-xl font-medium text-[#153a75]">
+                Dark Mode
+              </span>
+              <label
+                htmlFor="checkbox"
+                className="relative sm:inline-block w-10 h-5 ml-2 mt-[5px]"
+              >
+                <input
+                  type="checkbox"
+                  id="checkbox"
+                  className="opacity-0 w-0 h-0"
+                  onClick={() => console.log('clicked')}
+                />
+                <span className="slider round"></span>
+              </label>
+            </div>
           </div>
         </div>
       </nav>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    screens: {
+      'l-md': '860px',
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   plugins: [],
