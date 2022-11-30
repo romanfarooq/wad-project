ed236473bb166e4416aaa7c9e3e9b485acfa6e77
Roman Farooq <TheRomanFarooq@gmail.com>
2022-12-01 03:21:38 +0500

    styled navbar for darkmode

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,11 +10,11 @@ function Navbar() {
   const dispatch = useDispatch();
   const { darkMode } = useSelector((state) => state.darkMode);
   return (
-    <nav className="fixed top-0 bottom-0 left-0 w-1/5 bg-primary-10 rounded-r-3xl shadow-inner">
+    <nav className="fixed top-0 bottom-0 left-0 w-1/5 bg-primary-10 dark:bg-gray-800 rounded-r-3xl shadow-inner">
       <div className="flex m-5 pb-3 border-b-2 border-blue-100">
         <FcDocument className="text-5xl m-auto sm:m-0 md:text-4xl lg:text-5xl" />
-        <h1 className="hidden sm:block sm:text-sm md:text-base lg:text-lg xl:text-2xl m-auto ml-1 text-blue-900 font-extrabold">
-          TO-DO APP
+        <h1 className="hidden sm:block sm:text-sm md:text-base lg:text-lg xl:text-2xl m-auto ml-1 text-blue-900 dark:text-gray-200 font-extrabold">
+          TODO <span className="dark:text-blue-500">APP</span>
         </h1>
       </div>
       <div className="flex h-16 flex-col xl:flex-row sm:h-32 xl:h-24 justify-center items-center">
@@ -23,7 +23,7 @@ function Navbar() {
           src={ProfilePicture}
           alt="profile-pic"
         />
-        <div className="hidden sm:flex text-center xl:text-left flex-col justify-center text-blue-900">
+        <div className="hidden sm:flex text-center xl:text-left flex-col justify-center m-1 text-blue-900 dark:text-gray-200">
           <span className="font-semibold">Roman Farooq</span>
           <small className="hidden l-md:block text-xs">
             TheRomanFarooq@gmail.com
@@ -38,8 +38,8 @@ function Navbar() {
             end
             className={({ isActive }) =>
               isActive
-                ? "flex items-center gap-2 px-2 justify-center sm:justify-start md:px-5 py-2 rounded-r-md bg-[#ebecf5] border-r-8 border-[#234d92] text-blue-900 font-bold"
-                : "flex items-center gap-2 px-2 justify-center sm:justify-start md:px-5 py-2 rounded-r-md hover:bg-[#ebecf5] hover:border-r-8 transition duration-200 border-[#234d92] text-blue-900 font-medium hover:font-bold"
+                ? "flex items-center gap-2 px-2 justify-center sm:justify-start md:px-5 py-2 rounded-r-md bg-[#ebecf5] dark:bg-blue-900 border-r-8 border-[#234d92] dark:border-blue-500 text-blue-900 dark:text-gray-200 font-bold"
+                : "flex items-center gap-2 px-2 justify-center sm:justify-start md:px-5 py-2 rounded-r-md hover:bg-[#ebecf5] dark:hover:bg-blue-900 hover:border-r-8 transition duration-200 border-[#234d92] dark:border-blue-500 text-blue-900 dark:text-gray-200 font-medium hover:font-bold"
             }
           >
             {item.icon}
@@ -58,7 +58,7 @@ function Navbar() {
             />
           </div>
           <div className="hidden sm:flex">
-            <span className="block my-auto text-xs md:text-sm lg:text-base xl:text-xl font-medium text-blue-900">
+            <span className="block my-auto text-xs md:text-sm lg:text-base xl:text-xl font-medium text-blue-900 dark:text-gray-200">
               Dark Mode
             </span>
             <label
